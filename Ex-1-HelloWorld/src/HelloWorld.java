c95b09dd3d87f014c52b8185a355daba4ba640cd
SUDIPA SAHOO <SUDIPA SAHOO@LAPTOP-9I8O0A9K>
2020-08-13 12:42:37 +0530

    Adding the master program to the Udemy-Repository

---
+++ Ex-1-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		double x=1.05;
+		double y=2.55;
+		BigDecimal d1 = new BigDecimal("1.05");
+		BigDecimal d2 = new BigDecimal("1.55");
+		System.out.println(d1.add(d2));
+
+	}
+
+}
